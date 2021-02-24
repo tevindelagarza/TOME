9b8b4c40bd7d2a5cca95cc938576d07f43bb33aa
Ubuntu User <tevindelagarza>
2021-02-24 02:28:27 +0000

    TOMW

---
+++ catkin_ws/build/roboclaw_node/catkin_generated/setup_py_interrogation.cmake
@@ -0,0 +1,7 @@
+set(roboclaw_node_SETUP_PY_SETUP_MODULE "distutils.core")
+set(roboclaw_node_SETUP_PY_VERSION "0.0.1")
+set(roboclaw_node_SETUP_PY_SCRIPTS "")
+set(roboclaw_node_SETUP_PY_PACKAGES "roboclaw_driver")
+set(roboclaw_node_SETUP_PY_PACKAGE_DIRS "src/roboclaw_driver")
+set(roboclaw_node_SETUP_PY_MODULES "")
+set(roboclaw_node_SETUP_PY_MODULE_DIRS "")
